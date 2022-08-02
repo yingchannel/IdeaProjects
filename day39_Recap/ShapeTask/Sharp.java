faf36a7be882c0197e41192bdf422ac79151d95e
ying <annchen20092009@hotmail.com>
2022-08-02 17:12:15 +0200

    day39: Recap

---
+++ day39_Recap/ShapeTask/Sharp.java
@@ -0,0 +1,49 @@
+package day39_Recap.ShapeTask;
+
+public class Sharp {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name == null){
+            System.err.println("Name can not be null");
+            System.exit(1); // 1: something went wrong
+        }
+
+        // ""  != null
+        if(name.isEmpty() || name.isBlank()){
+            System.err.println("Invalid name");
+            System.exit(1);
+        }
+        this.name = name;
+    }
+
+    public Sharp(String name) {
+        setName(name);
+    }
+
+    public double area(){
+        return 0;
+    }
+
+    public double perimeter(){
+        return 0;
+    }
+}
+/*
+Shape:
+	variables:
+			name
+
+	Encapsulate the field
+
+	Add a constructor to set the filed
+
+	Methods:
+		area(){}
+		perimeter(){}
+ */
