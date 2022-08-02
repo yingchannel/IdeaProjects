4fe00ddca0f69a8571d7c11a486ab4484538202e
ying <annchen20092009@hotmail.com>
2022-08-02 02:13:28 +0200

    ParkingLot

---
+++ day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,13 @@
+package day38_Inheritance.carTask;
+
+public class ParkingLot {
+
+    public static void main(String[] args) {
+
+        Toyota toyota = new Toyota("Camry", 2020, 20000, "Gray", 1234556);
+
+        Tesla tesla = new Tesla("Model S", 2022, 98990, "Red",0);
+
+        BMW bmw = new BMW("X5",2020, 45623, "Black", 12000);
+    }
+}
