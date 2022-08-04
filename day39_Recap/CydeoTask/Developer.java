fed3a167b18128e2ddc934bbce4e002805d24744
ying <annchen20092009@hotmail.com>
2022-08-05 01:06:17 +0200

    day39: Recap

--- day39_Recap/CydeoTask/Developer.java
+++ day39_Recap/CydeoTask/Developer.java
@@ -1,8 +1,19 @@
 package day39_Recap.CydeoTask;
 
-public class Developer {
+public class Developer extends Employee{
 
+    public Developer(String name, int age, char gender, int employeeId, String jobTitle, double salary) {
+        super(name, age, gender, employeeId, jobTitle, salary);
+    }
 
+    public void fixingBugs(){
+        System.out.println( getJobTitle() + " " + getName() + " is fixing the bugs");
+    }
+
+    @Override
+    public void work() {
+        System.out.println( getJobTitle() + " " + getName() + " is developing the app");
+    }
 }
 /*
 4. Create a subclass of Employee named Developer
