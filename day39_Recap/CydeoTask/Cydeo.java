8f8df764cf6992568d2e54b9a77f4c9b5833cf37
ying <annchen20092009@hotmail.com>
2022-08-05 01:43:35 +0200

    day39: Recap

---
+++ day39_Recap/CydeoTask/Cydeo.java
@@ -0,0 +1,82 @@
+package day39_Recap.CydeoTask;
+
+public class Cydeo {
+
+    public static void main(String[] args) {
+
+        Developer developer = new Developer("Korkmaz", 29, 'M', 1, "Java Devloper", 125000);
+
+        Tester tester = new Tester("Olga", 27, 'F', 2, "SDET", 100000);
+
+        Teacher teacher = new Teacher("Daniel", 32, 'M', 3, "Math Teacher", 100000);
+
+        Student student = new Student("Suhaib", 30, 'M', 8, "SDET");
+
+        System.out.println(developer);
+        System.out.println(tester);
+        System.out.println(teacher);
+        System.out.println(student);
+
+        System.out.println("------------------");
+
+        developer.setAge(39);
+
+        System.out.println(developer.getAge());
+
+        System.out.println(developer);
+
+        System.out.println("--------------------");
+
+        developer.work();
+        tester.work();
+        teacher.work();
+
+        System.out.println("---------------------");
+
+        developer.eat();
+        developer.drink();
+        developer.sleep();
+
+        developer.fixingBugs();
+
+        System.out.println("----------------");
+
+        tester.eat();
+        tester.drink();
+        tester.sleep();
+        //tester.fixingBugs();
+        tester.createTicket();
+
+        System.out.println("----------------");
+
+        teacher.eat();
+        teacher.drink();
+        teacher.sleep();
+
+        //teacher.createTicket();
+        System.out.println("----------------");
+
+        student.eat();
+        student.drink();
+        student.sleep();
+
+        //student.fixingBugs();
+        //student.createTicket();
+
+        student.study();
+
+
+
+    }
+
+
+
+}
+/*
+7. Create a class named Cydeo:
+            create objects of Tester, Developer,Teacher, Student
+
+            test all the functions of each objects
+
+            Analyze the relationships between the classes
+ */
