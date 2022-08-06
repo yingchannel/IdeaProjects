365d641779b0d9a2676cf490f50a044700dea0d0
ying <annchen20092009@hotmail.com>
2022-08-06 02:44:43 +0200

    day41: Exception

---
+++ day41_Exceptions/FinallyBlock.java
@@ -0,0 +1,29 @@
+package day41_Exceptions;
+
+public class FinallyBlock {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3};
+
+        try{
+            System.out.println(arr[10]);
+            System.out.println("Try Block");
+        }catch(RuntimeException e){
+            System.out.println("Catch Block");
+            e.printStackTrace();
+            System.exit(0); // the only way to  stop finally block
+        }finally{
+            System.out.println("Finally Block");// always gets executed
+
+        }
+
+
+
+
+
+    }
+
+
+
+}
