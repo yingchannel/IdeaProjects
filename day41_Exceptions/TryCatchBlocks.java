c3046fc65dcad714cf94585fc0fac8aece7f61bd
ying <annchen20092009@hotmail.com>
2022-08-06 02:04:54 +0200

    day41: Exception

--- day41_Exceptions/TryCatchBlocks.java
+++ day41_Exceptions/TryCatchBlocks.java
@@ -1,5 +1,8 @@
 package day41_Exceptions;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class TryCatchBlocks {
 
     public static void main(String[] args) {
@@ -50,5 +53,27 @@ public class TryCatchBlocks {
             e.printStackTrace();
         }
         System.out.println("Test3 completed");
+
+        System.out.println("-------------------");
+
+        System.out.println("Hello");
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Cydeo");
+
+        System.out.println("-------------------");
+
+        try {
+            FileInputStream file = new FileInputStream("File path");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
