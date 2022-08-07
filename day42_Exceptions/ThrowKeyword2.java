6513d08f60967853d7475b7f1121cd58dc0530c0
ying <annchen20092009@hotmail.com>
2022-08-07 23:29:58 +0200

    day42: Exception

---
+++ day42_Exceptions/ThrowKeyword2.java
@@ -0,0 +1,21 @@
+package day42_Exceptions;
+
+import day04_Variable.Rectangle;
+
+import java.util.NoSuchElementException;
+
+public class ThrowKeyword2 {
+
+    public static void main(String[] args) {
+
+        throw new NoSuchElementException("There is no such element as break");
+
+        // throw new InterruptedException();
+
+        //System.out.println("Hello world");
+
+        //throw new Rectangle(5,7);
+
+
+    }
+}
