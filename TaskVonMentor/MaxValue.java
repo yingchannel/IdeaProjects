b8446aa488b991f1aa24ed2f702a8bb09289ca28
ying <annchen20092009@hotmail.com>
2022-08-08 14:38:38 +0200

    Task von Metor

---
+++ TaskVonMentor/MaxValue.java
@@ -0,0 +1,26 @@
+package TaskVonMentor;
+
+import java.util.Scanner;
+
+public class MaxValue {
+
+    public static void main(String[] args) {
+
+        int [] arr = {-1,-2,-3,-4,-5,-6};
+
+        int max = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if(arr[i] > max){
+                max = arr[i];
+            }
+
+        }
+        System.out.println(max);
+
+    }
+}
+/*
+How can you find max value in an int array?
+ */
