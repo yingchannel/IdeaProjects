2eea1beeb8c798c8a51c53104da55270d1d99e4d
ying <annchen20092009@hotmail.com>
2022-08-21 20:26:05 +0200

    day 45 abstraction Interface

--- day45_Abstraction_Interface/shape/Circle.java
+++ day45_Abstraction_Interface/shape/Circle.java
@@ -1,4 +1,45 @@
 package day45_Abstraction_Interface.shape;
 
-public class Circle {
+public class Circle extends Shape{
+
+    private double radius;
+
+    public final static double pi = 3.14;
+
+    public Circle(double radius) {
+        super("Circle");
+        setRadius(radius);
+    }
+
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if(radius < 0){
+            throw new RuntimeException("Invalid Radius: " + radius);
+        }
+
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return radius * radius * pi;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2* radius * pi;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                super.toString()+
+                "radius=" + radius +
+                '}';
+    }
 }
--- day45_Abstraction_Interface/shape/Square.java
+++ day45_Abstraction_Interface/shape/Square.java
@@ -1,4 +1,40 @@
 package day45_Abstraction_Interface.shape;
 
-public class Square {
+public class Square extends Shape{
+
+    private double side;
+
+    public Square(double side) {
+        super("Square");
+        setSide(side);
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        if(side<0){
+            throw new RuntimeException("Invalid side : "+ side);
+        }
+        this.side = side;
+    }
+
+    @Override
+    public double area() {
+        return 0;
+    }
+
+    @Override
+    public double perimeter() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                super.toString()+
+                "side=" + side +
+                '}';
+    }
 }
